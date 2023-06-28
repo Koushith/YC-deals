60f904e295071c238e4cce9255b98d1de7448daa
koushith <koushith97@gmail.com>
2023-06-28 23:06:42 +0530

    removes cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,13 +25,19 @@ const app: Express = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "*",
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || ALLOWED_ORIGINS.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   })
+// );
+
+app.use(cors())
 
 //Reclaim related routes
 app.post("/home", home);
